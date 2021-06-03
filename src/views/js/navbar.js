770752a41dab099fdd5db779b1e5bbf8b7a64b26
Anthony-jcb <thonyjbc@gmail.com>
2021-06-03 16:56:26 -0430

    Se eliminó portfolio.html & .css, se creó "assets"
    Se quitaron variables innecesarias de navbar.js
    
    Ahora el contenido visual se encuentra en "assets"
    Se editó el html, lo que antes era "nav" ahora es <header> y se le
    quitaron elementos innecesarios
    
    Los íconos del header ahora están incrustados dentro del html para mayor
    eficiencia
    
    Se borró la etiqueta <link> con la fuente de íconos por ser innecesaria
    
    Se añadió un footer

--- src/views/js/navbar.js
+++ src/views/js/navbar.js
@@ -1,17 +1,10 @@
 let navbar = document.getElementById("nav");
-let nav_brand = document.getElementById("nav_brand");
-let navbar_options = document.getElementById("nav_options");
-let sticky = navbar.offsetTop;
 
 function stickyMenu() {
   if (window.pageYOffset > 0.0) {
     navbar.classList.add("sticky");
-    nav_brand.classList.add("sticky_option");
-    navbar_options.classList.add("sticky_option");
   } else {
     navbar.classList.remove("sticky");
-    nav_brand.classList.remove("sticky_option");
-    navbar_options.classList.remove("sticky_option");
   }
 }
 
