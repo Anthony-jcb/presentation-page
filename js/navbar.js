25c1a9b70e5eceb96f65d63b65737642f1bc75af
Anthony-jcb <thonyjbc@gmail.com>
2021-06-09 01:24:25 -0430

    Se añadió responsive al nav

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,19 @@
-let navbar=document.getElementById("nav");function stickyMenu(){if(window.pageYOffset>0.0){navbar.classList.add("sticky")}else{navbar.classList.remove("sticky")}}window.onscroll=function(){stickyMenu()};
+const navbar = document.getElementById("nav");
+const menu_btn = document.getElementById("menu_btn");
+const menu_items = document.getElementById("menu_items");
+
+function stickyMenu() {
+  if (window.pageYOffset > 0.0) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.onscroll = () => stickyMenu();
+
+addEventListener("DOMContentLoaded", () => {
+  menu_btn.onclick = () => {
+    menu_items.classList.toggle("show");
+  };
+});
