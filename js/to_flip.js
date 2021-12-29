2fd8d77432869a225be8125c87e445323c1da91f
aithonai <thonyjbc@gmail.com>
2021-12-29 15:53:03 -0400

    Contact section finished.

--- js/to_flip.js
+++ js/to_flip.js
@@ -2,6 +2,6 @@ const D = document
 export default function toFlip(sections) {
   const $sections = D.querySelectorAll(sections)
   $sections.forEach((section, i) => {
-    if (i % 2 == 0) section.classList.add("flip")
+    if (i % 2 !== 0) section.classList.add("flip")
   })
 }
